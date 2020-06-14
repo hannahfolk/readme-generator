4494cce48f3e492b598bec156d4a2bc029628ca0
hfolk25 <hfolk25@gmail.com>
2020-06-14 02:58:26 -0700

    changing App.js and adding in UserInputForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header, Container, Card } from "semantic-ui-react";
+import UserInputForm from "./components/UserInputForm";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Header inverted as="h1" block textAlign="center">
+        README Generator
+      </Header>
+      <Card fluid raised id="form">
+        <UserInputForm />
+      </Card>
+    </Container>
   );
 }
 
