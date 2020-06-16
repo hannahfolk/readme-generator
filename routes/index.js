1e4a6890240e61f5525904d04baedbcfd721e718
hfolk25 <hfolk25@gmail.com>
2020-06-16 01:40:50 -0700

    add generateLicense functionality

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,24 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
+const generateLicense = require("../controllers/generateLicense");
 const generateREADME = require("../controllers/generateREADME");
 
 const OUTPUT_DIR = path.resolve(__dirname, "../output");
-const outputPath = path.join(OUTPUT_DIR, "README.md");
+const outputPathReadME = path.join(OUTPUT_DIR, "README.md");
+const outputPathLicense = path.join(OUTPUT_DIR, "LICENSE.txt");
 
-router.post("/generateREADME", (req, res) => {
+router.post("/generate", (req, res) => {
   const { formObject } = req.body;
 
+  const licenseStr = generateLicense(formObject);
   const readMeStr = generateREADME(formObject);
 
   if (!fs.existsSync(OUTPUT_DIR)) {
     fs.mkdirSync(OUTPUT_DIR);
   }
-  fs.writeFileSync(outputPath, readMeStr, "utf-8");
+  fs.writeFileSync(outputPathLicense, licenseStr, "utf-8");
+  fs.writeFileSync(outputPathReadME, readMeStr, "utf-8");
 });
 
 module.exports = router;
