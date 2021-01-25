8ada210187d2fcc63096a2e79e171a10b23af874
hfolk25 <hfolk25@gmail.com>
2021-01-25 14:05:49 -0800

    fixing capitalization

--- routes/index.js
+++ routes/index.js
@@ -7,20 +7,23 @@ const generateLicense = require("../controllers/generateLicense");
 const generateREADME = require("../controllers/generateREADME");
 
 const OUTPUT_DIR = path.resolve(__dirname, "../output");
-const outputPathReadME = path.join(OUTPUT_DIR, "README.md");
+const outputPathREADME = path.join(OUTPUT_DIR, "README.md");
 const outputPathLicense = path.join(OUTPUT_DIR, "LICENSE.txt");
 
 router.post("/generate", (req, res) => {
-  const { formObject } = req.body;
+  const { formObject, techState, ackState } = req.body;
 
   const licenseStr = generateLicense(formObject);
-  const readMeStr = generateREADME(formObject);
+  const READMEStr = generateREADME(formObject, techState, ackState);
+  console.log(READMEStr);
 
   if (!fs.existsSync(OUTPUT_DIR)) {
     fs.mkdirSync(OUTPUT_DIR);
   }
   fs.writeFileSync(outputPathLicense, licenseStr, "utf-8");
-  fs.writeFileSync(outputPathReadME, readMeStr, "utf-8");
+  fs.writeFileSync(outputPathREADME, READMEStr, "utf-8");
+
+  console.log("Success!");
 });
 
 module.exports = router;
